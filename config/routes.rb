31a44bc3fad3075e5a5e88bb342054bcc5d1d7b8
Nezzie <rodneycordoba@gmail.com>
2024-02-24 21:16:22 -0500

    Ruby on Rails testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "posts/new"
+  post "posts" => "posts#create"
+  get "posts/:id" => "posts#show"
+  # Post new testing
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
